9aac015c553ec1ece6cf09f8c37f6dd2aea66db0
Gaosheng Liu <93236788+GaoshengLiu@users.noreply.github.com>
2026-04-22 22:34:41 +0800

    Add files via upload

---
+++ Inference.sh
@@ -0,0 +1,7 @@
+# ##Inference mine
+python Inference.py --dataset ./Data/Train/TB_Raytrix --output_path Raytrix_My01 -m Net_context -p ./updatedCheckpoint/MyLocGloCTX01_new.tar --patch 384
+python Inference.py --dataset ./Data/Train/TB_Raytrix --output_path Raytrix_My005 -m Net_context -p ./updatedCheckpoint/MyLocGloCTX005_new.tar --patch 384
+python Inference.py --dataset ./Data/Train/TB_Raytrix --output_path Raytrix_My0025 -m Net_context -p ./updatedCheckpoint/MyLocGloCTX0025_new.tar --patch 384
+python Inference.py --dataset ./Data/Train/TB_Raytrix --output_path Raytrix_My001 -m Net_context -p ./updatedCheckpoint/MyLocGloCTX001_new.tar --patch 384
+python Inference.py --dataset ./Data/Train/TB_Raytrix --output_path Raytrix_My0005 -m Net_context -p ./updatedCheckpoint/MyLocGloCTX0005_new.tar --patch 384
+python Inference.py --dataset ./Data/Train/TB_Raytrix --output_path Raytrix_My0001 -m Net_context -p ./updatedCheckpoint/MyLocGloCTX0001_new.tar --patch 384
